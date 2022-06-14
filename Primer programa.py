70b8c0ade520e8039e4e8a0ba59d20cd514b0bb2
Facuib <ibarrafacundo@outlook.com>
2022-06-14 01:18:48 +0000

    1er commit

--- Primer programa.py
+++ Primer programa.py
@@ -1,1 +1,9 @@
 #Este es mi primer programa 1.0
+
+#La consiga uno es= definir una funcion que permita ingresar dos valores enteros
+#
+def ingresovalores ():
+  nro1=int(input("ingrese un valor entero: "))
+  nro2=int(input("Ingrese otro valor entero: "))
+  print("Los numeros ingresados son: ",nro1,'y',nro2,'.')
+ingresovalores()
